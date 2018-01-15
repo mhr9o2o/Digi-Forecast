2ab74361b014b5d37380f969818dc20030afa0be
Mohammad <rahnama.m.h90@gmail.com>
2018-01-15 14:35:54 +0330

    adding packages and moving MainActivity to "activities" package

--- app/src/main/java/digi/mhr/digiforecast/activities/MainActivity.java
+++ app/src/main/java/digi/mhr/digiforecast/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package digi.mhr.digiforecast;
+package digi.mhr.digiforecast.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import digi.mhr.digiforecast.R;
 
 public class MainActivity extends AppCompatActivity {
 
