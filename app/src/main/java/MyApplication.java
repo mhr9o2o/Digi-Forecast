a6d2e6a60f6ae7d927e473d76518ee2f349016c7
Mohammad <rahnama.m.h90@gmail.com>
2018-01-16 16:20:18 +0330

    implementing custom Application class and initializing Realm instance

---
+++ app/src/main/java/MyApplication.java
@@ -0,0 +1,16 @@
+import android.app.Application;
+
+import io.realm.Realm;
+
+/**
+ * Created by mohammad on 1/16/18.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
