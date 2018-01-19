c79e8c2d574a444fa90cfb31624746e6eb75cfa4
Mohammad <rahnama.m.h90@gmail.com>
2018-01-19 18:19:07 +0330

    binding main activity views

--- app/src/main/java/digi/mhr/digiforecast/activities/MainActivity.java
+++ app/src/main/java/digi/mhr/digiforecast/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package digi.mhr.digiforecast.activities;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -18,9 +21,54 @@ import digi.mhr.digiforecast.network.responses.GetCurrentWeatherResponse;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+     * Views:
+     */
+    /*
+     * * Weather Detail Views:
+     */
+    private TextView maxTemperatureTV;
+    private TextView minTemperatureTV;
+    private TextView currentTemperatureTV;
+    private ImageView currentConditionIV;
+    private TextView conditionDescriptionTV;
+    private TextView humidityTV;
+    private TextView windVelocityTV;
+    private TextView windDegreeTV;
+    /*
+     * * Activity Views:
+     */
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private TextView regionInfoTV;
+    private TextView lastUpdateTV;
+
+    /*
+     * Activity Functions:
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindViews();
     }
+
+    /*
+     * Binding Functions:
+     */
+    private void bindViews() {
+        maxTemperatureTV = findViewById(R.id.item_weather_detail_max_temp);
+        minTemperatureTV = findViewById(R.id.item_weather_detail_min_temp);
+        currentTemperatureTV = findViewById(R.id.item_weather_detail_temp);
+        currentConditionIV = findViewById(R.id.item_weather_detail_icon);
+        conditionDescriptionTV = findViewById(R.id.item_weather_detail_description);
+        humidityTV = findViewById(R.id.item_weather_detail_humidity);
+        windVelocityTV = findViewById(R.id.item_weather_detail_wind_velocity);
+        windDegreeTV = findViewById(R.id.item_weather_detail_wind_degree);
+        //
+        swipeRefreshLayout = findViewById(R.id.activity_main_refresh_layout);
+        regionInfoTV = findViewById(R.id.activity_main_region_info);
+        lastUpdateTV = findViewById(R.id.activity_main_last_update);
+    }
+
 }
